5e9913efac75abb6cb0dc052a4ed49be90e76552
Lev E. Chechulin <l.e.chechulin@gmail.com>
2021-04-05 17:38:57 +0200

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,5 +5,30 @@
 int
 main()
 {
-fork
+    int p_1[2];
+    int p_2[2];
+    char buf[8];
+    //int cur_pid;
+
+
+    pipe(p_1);
+    pipe(p_2);
+
+    write(p_1[2], "1", 8);
+
+    int pid = fork();
+    if (pid == 0) {
+        char buf[8];
+        int cur_pid = getpid();
+        read(p_1[1], buf, sizeof buf);
+        printf(pid ":received ping");
+        write(p_2[2], buf, sizeof buf);
+        exit(0);
+    }
+
+    int par_cur_pid = getpid();
+
+    read(p_2[1], buf, sizeof buf);
+    printf(pid ":received pong");
+    exit(0);
 }
